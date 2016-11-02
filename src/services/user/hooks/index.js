61bcc4b63ab550afb062fc441006ad4b6d7c1604
dmitrovskiy <vladimir@tep.io>
2016-11-03 00:44:46 +0200

    Feat: remove unnecessary services

--- src/services/user/hooks/index.js
+++ src/services/user/hooks/index.js
@@ -1,47 +1,17 @@
 'use strict';
 
-const globalHooks = require('../../../hooks');
-const hooks = require('feathers-hooks');
-const auth = require('feathers-authentication').hooks;
-
 exports.before = {
   all: [],
-  find: [
-    auth.verifyToken(),
-    auth.populateUser(),
-    auth.restrictToAuthenticated()
-  ],
-  get: [
-    auth.verifyToken(),
-    auth.populateUser(),
-    auth.restrictToAuthenticated(),
-    auth.restrictToOwner({ ownerField: '_id' })
-  ],
-  create: [
-    auth.hashPassword()
-  ],
-  update: [
-    auth.verifyToken(),
-    auth.populateUser(),
-    auth.restrictToAuthenticated(),
-    auth.restrictToOwner({ ownerField: '_id' })
-  ],
-  patch: [
-    auth.verifyToken(),
-    auth.populateUser(),
-    auth.restrictToAuthenticated(),
-    auth.restrictToOwner({ ownerField: '_id' })
-  ],
-  remove: [
-    auth.verifyToken(),
-    auth.populateUser(),
-    auth.restrictToAuthenticated(),
-    auth.restrictToOwner({ ownerField: '_id' })
-  ]
+  find: [],
+  get: [],
+  create: [],
+  update: [],
+  patch: [],
+  remove: []
 };
 
 exports.after = {
-  all: [hooks.remove('password')],
+  all: [],
   find: [],
   get: [],
   create: [],
