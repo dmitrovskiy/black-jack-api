7dfc9f4f56c5e07adbd5de9ca778342b13e09eb7
dmitrovskiy <vladimir@tep.io>
2016-11-03 00:36:50 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const pkginfo = require('../package');
+const app = require('./app');
+const port = app.get('port');
+const server = app.listen(port);
+
+server.on('listening', () =>
+  console.log(`${pkginfo.name} application started on ${app.get('host')}:${port}`)
+);
