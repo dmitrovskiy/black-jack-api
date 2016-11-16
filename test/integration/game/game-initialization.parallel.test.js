aaaf7c2dbe979fd67d2e07d1ff0b2b4b652cdeab
dmitrovskiy <vladimir@tep.io>
2016-11-16 19:29:54 +0200

    Fix: game clean up

--- test/integration/game/game-initialization.parallel.test.js
+++ test/integration/game/game-initialization.parallel.test.js
@@ -6,11 +6,13 @@ import {assert} from 'chai';
 import randomString from 'randomstring';
 
 import userModel from '../../../src/services/user/user-model';
-import gameModel from '../../../src/services/game/';
+import gameModel from '../../../src/services/game/game-model';
 
 const userStub = {
   email: `${randomString.generate(12)}@test.com`
 };
+
+const gameStub = {};
 
 test.before('prepare a user', async t => {
   await userModel.remove();
@@ -25,6 +27,10 @@ test.after('clean up a user', async t => {
   await userModel.remove({email: userStub.email});
 });
 
+test.after('clean up a game', async t => {
+  await gameModel.remove({_id: gameStub.id});
+});
+
 test('game: should be initialized', async t => {
   await request
     .post('/games')
@@ -33,8 +39,12 @@ test('game: should be initialized', async t => {
       bet: 10
     })
     .expect(res => {
+      assert.isDefined(res.body.id);
       assert.isDefined(res.body.clientCards);
       assert.isDefined(res.body.dealerCards);
     })
-    .expect(201);
+    .expect(201)
+    .then(res => {
+      gameStub.id = res.body.id;
+    });
 });
